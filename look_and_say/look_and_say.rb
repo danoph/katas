1e8cbd359243e60e7c71f099127238e57204b72b
Daniel Errante <daniel.errante@asynchrony.com>
2015-02-16 11:09:03 -0600

    Solution without regular expression. Kenny

--- look_and_say/look_and_say.rb
+++ look_and_say/look_and_say.rb
@@ -1,15 +1,36 @@
 class LookAndSay
-  
-  def initialize(string)
-    # Implement me!
+
+  def initialize(items)
+    if items.kind_of? Array
+      @sequence = items.collect(&:to_s).join('')
+    else
+      @sequence = items
+    end
+    @says = []
+    @next_items = ''
+    array = @sequence.split('')
+    i = 0
+    while i < array.length do
+      current = array[i]
+      unless (current.to_i.to_s == current) then
+        raise ArgumentError.new("Should not allow strings with non-digit characters")
+      end
+      count = 1
+      i += 1
+      while current == array[i] do
+        count += 1
+        i += 1
+      end
+      @next_items += "#{count}#{current}"
+    end
   end
 
   def next
-    # Implement me!
+    LookAndSay.new(@next_items)
   end
 
   def to_s
-    # Implement me!
+    @sequence
   end
-  
+
 end
--- look_and_say/look_and_say_test.rb
+++ look_and_say/look_and_say_test.rb
@@ -1,50 +1,53 @@
-require 'test/unit'
+require 'minitest/autorun'
 require_relative 'look_and_say'
 
-class LookAndSayTest < Test::Unit::TestCase
+class LookAndSayTest < Minitest::Test
 
   #########################
   #                       #
   # LookAndSay#initialize #
   #                       #
   #########################
-  
+
   def test_initialize_fails_on_non_numbers
-    assert_raise(ArgumentError, "Should not allow strings with non-digit characters") do
+    assert_raises(ArgumentError, "Should not allow strings with non-digit characters") do
       LookAndSay.new("foo")
-    end    
+    end
   end
 
   def test_initialize_stringifies_what_is_passed_in
-    assert_nothing_raised(ArgumentError, "Should stringify what is passed in") do
+    begin
       LookAndSay.new [1, 2, 3]
+      assert_equal true, true
+    rescue ArgumentError => e
+      fail("Should stringify what is passed in, error: #{e}")
     end
   end
-  
+
   ###################
   #                 #
   # LookAndSay#to_s #
   #                 #
   ###################
-  
+
   def test_to_s_returns_a_string
     assert_kind_of String, LookAndSay.new("1234").to_s
   end
-  
+
   def test_to_s_returns_same_string_value_if_next_is_not_called
     assert_equal "123454321", LookAndSay.new("123454321").to_s
   end
-  
+
   ###################
   #                 #
   # LookAndSay#next #
   #                 #
   ###################
-  
+
   def test_next_returns_a_look_and_say_object
     assert_kind_of LookAndSay, LookAndSay.new("1234").next
   end
-  
+
   def test_1_returns_11
     assert_equal "11", LookAndSay.new("1").next.to_s
   end
@@ -52,17 +55,17 @@ class LookAndSayTest < Test::Unit::TestCase
   def test_1_returns_21_after_2_steps
     assert_equal "21", LookAndSay.new("1").next.next.to_s
   end
-  
+
   def test_1_returns_1211_after_3_steps
     assert_equal "1211", LookAndSay.new("1").next.next.next.to_s
   end
-  
+
   def test_22_is_a_fixed_point
     assert_equal "22", LookAndSay.new("22").next.to_s
   end
-  
+
   def test_a_complicated_case
-    assert_equal "11131432134114252442211311122322", LookAndSay.new("13422231111455442222113123322").next.to_s  
+    assert_equal "11131432134114252442211311122322", LookAndSay.new("13422231111455442222113123322").next.to_s
   end
-  
+
 end
