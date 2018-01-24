db65f5809d110d9decb04fb2d834be867e6371f3
Daniel Errante <danoph@gmail.com>
2018-01-24 11:28:00 -0600

    Added checks for too many and too few cards

--- texas_hold_em/texas_hold_em.rb
+++ texas_hold_em/texas_hold_em.rb
@@ -1,11 +1,17 @@
 class TexasHoldEm
-  
+
   def initialize(cards)
     # Implement me!
+    cards = cards.split(' ')
+    if cards.length > 7
+      raise(ArgumentError, 'Should not accept more than 7 cards')
+    elsif cards.length < 7
+      raise(ArgumentError, 'Should not accept fewer than 7 cards')
+    end
   end
-  
+
   def best_hand
     # Implement me!
   end
-  
+
 end
