4dc921029f6a2db84904f0f6312a184ecc49b185
Daniel Errante <danoph@gmail.com>
2018-01-24 12:07:26 -0600

    added card validation

--- texas_hold_em/texas_hold_em.rb
+++ texas_hold_em/texas_hold_em.rb
@@ -1,5 +1,26 @@
 class TexasHoldEm
 
+  def validate_cards(cards)
+
+    valid_values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+
+    valid_suits = ['H','C','S','D']
+
+    valid_cards = valid_suits.map do |suit|
+      valid_values.map  do |value|
+        "#{ value }#{ suit }"
+      end
+    end.flatten
+
+    cards.each do |card|
+      card_found = valid_cards.find{|valid_card| card == valid_card }
+      if !card_found
+        raise(ArgumentError, "Should not accept #{ card }")
+      end
+    end
+
+  end
+
   def initialize(cards)
     # Implement me!
     cards = cards.split(' ')
@@ -13,6 +34,8 @@ class TexasHoldEm
     if cards.uniq.length != 7
       raise(ArgumentError, 'Should not accept duplicated cards')
     end
+
+    validate_cards(cards)
 
   end
 
