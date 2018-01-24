ff9ff198a2186fcd38879ba190967e465d0faabb
Daniel Errante <danoph@gmail.com>
2018-01-24 11:39:48 -0600

    added dup check

--- texas_hold_em/texas_hold_em.rb
+++ texas_hold_em/texas_hold_em.rb
@@ -3,11 +3,17 @@ class TexasHoldEm
   def initialize(cards)
     # Implement me!
     cards = cards.split(' ')
+
     if cards.length > 7
       raise(ArgumentError, 'Should not accept more than 7 cards')
     elsif cards.length < 7
       raise(ArgumentError, 'Should not accept fewer than 7 cards')
     end
+
+    if cards.uniq.length != 7
+      raise(ArgumentError, 'Should not accept duplicated cards')
+    end
+
   end
 
   def best_hand
