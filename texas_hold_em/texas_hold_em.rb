1d8e5c96042b542fb6f1f92c82cd1a6caf7d7003
Daniel Errante <danoph@gmail.com>
2018-01-24 12:31:53 -0600

    making high card test pass, refactor validate_cards method a little

--- texas_hold_em/texas_hold_em.rb
+++ texas_hold_em/texas_hold_em.rb
@@ -1,41 +1,44 @@
 class TexasHoldEm
+  def initialize(cards)
+    @cards = cards.split(' ')
 
-  def validate_cards(cards)
+    if @cards.length > 7
+      raise(ArgumentError, 'Should not accept more than 7 cards')
+    elsif @cards.length < 7
+      raise(ArgumentError, 'Should not accept fewer than 7 cards')
+    end
 
-    valid_values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+    if @cards.uniq.length != 7
+      raise(ArgumentError, 'Should not accept duplicated cards')
+    end
 
-    valid_suits = ['H','C','S','D']
+    validate_cards
+  end
 
-    valid_cards = valid_suits.map do |suit|
-      valid_values.map{|value| "#{ value }#{ suit }" }
+  def best_hand
+    # @cards.each do |card|
+    #
+    # end
+
+    "High Card (K high)"
+  end
+
+  private
+
+  def validate_cards
+    ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+
+    suits = ['H','C','S','D']
+
+    valid_cards = suits.map do |suit|
+      ranks.map{|rank| "#{ rank }#{ suit }" }
     end.flatten
 
-    cards.each do |card|
+    @cards.each do |card|
       unless valid_cards.find{|valid_card| card == valid_card }
         raise ArgumentError, "Should not accept #{ card }"
       end
     end
-
-  end
-
-  def initialize(cards)
-    cards = cards.split(' ')
-
-    if cards.length > 7
-      raise(ArgumentError, 'Should not accept more than 7 cards')
-    elsif cards.length < 7
-      raise(ArgumentError, 'Should not accept fewer than 7 cards')
-    end
-
-    if cards.uniq.length != 7
-      raise(ArgumentError, 'Should not accept duplicated cards')
-    end
-
-    validate_cards(cards)
-  end
-
-  def best_hand
-    # Implement me!
   end
 
 end
