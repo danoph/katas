e4eed46f486c6d3a96de480d150ceffe5c214531
Daniel Errante <danoph@gmail.com>
2018-02-01 14:11:17 -0600

    not allowing less than or more than 7 cards

--- texas_hold_em/texas_hold_em.rb
+++ texas_hold_em/texas_hold_em.rb
@@ -2,6 +2,8 @@ class TexasHoldEm
   VALID_RANKS = %w(2 3 4 5 6 7 8 9 10 J Q K A)
   VALID_SUITS = %w(D C S H)
 
+  VALID_NUMBER_OF_CARDS = 7
+
   def initialize(card_string)
     cards = card_string.split ' '
 
@@ -11,5 +13,8 @@ class TexasHoldEm
 
       raise ArgumentError, "Should not accept #{ card }" unless VALID_RANKS.include?(rank) && VALID_SUITS.include?(suit)
     end
+
+    raise ArgumentError, "Should not accept more than 7 cards" if cards.length > VALID_NUMBER_OF_CARDS
+    raise ArgumentError, "Should not accept fewer than 7 cards" if cards.length < VALID_NUMBER_OF_CARDS
   end
 end
