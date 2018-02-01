046dfb0a417ba57dccfa0466c376256e3db37bfe
Daniel Errante <danoph@gmail.com>
2018-02-01 14:00:18 -0600

    converting test unit cases to rspec

--- texas_hold_em/texas_hold_em_test.rb
+++ texas_hold_em/texas_hold_em_test.rb
@@ -1,165 +1,156 @@
 require 'test/unit'
 require './texas_hold_em'
 
-class TexasHoldEmTest < Test::Unit::TestCase
+describe TexasHoldEm do
+  subject { described_class.new cards }
 
-  ##############
-  #            #
-  # Validation #
-  #            #
-  ##############
+  describe 'bad cards' do
+    let(:cards) { "2C 3C 4C 5C 6C 7C #{ bad_card }" }
 
-  BAD_CARDS = %w( 1H 2J )
+    describe 'bad card 1' do
+      let(:bad_card) { '1H' }
 
-  def test_invalid_card
-    BAD_CARDS.each do |bad_card|
-      assert_raise(ArgumentError, "Should not accept #{ bad_card }") do
-        TexasHoldEm.new("2C 3C 4C 5C 6C 7C #{ bad_card }")
+      it 'does not allow bad cards' do
+        expect{ subject }.to raise_error(ArgumentError, "Should not accept #{ bad_card }")
+      end
+    end
+
+    describe 'bad card 2' do
+      let(:bad_card) { '2J' }
+
+      it 'does not allow bad cards' do
+        expect{ subject }.to raise_error(ArgumentError, "Should not accept #{ bad_card }")
       end
     end
   end
 
-  def test_too_many_cards
-    assert_raise(ArgumentError, "Should not accept more than 7 cards") do
-      TexasHoldEm.new("KD 9H 10D AD JD 6S QD 6D")
+  describe 'too many cards' do
+    let(:cards) { "KD 9H 10D AD JD 6S QD 6D" }
+
+    it 'does not allow more than 7 cards' do
+      expect{ subject }.to raise_error(ArgumentError, "Should not accept more than 7 cards")
     end
   end
 
-  def test_too_few_cards
-    assert_raise(ArgumentError, "Should not accept fewer than 7 cards") do
-      TexasHoldEm.new("KD 9H 10D AD JD 6S")
+  describe 'too few cards' do
+    let(:cards) { "KD 9H 10D AD JD 6S" }
+
+    it 'does not allow less than 7 cards' do
+      expect{ subject }.to raise_error(ArgumentError, "Should not accept fewer than 7 cards")
+    end
+
+    describe 'duplicate cards' do
+      let(:cards) { "KD 9H 10D AD JD 6S 9H" }
+
+      it 'does not allow duplicate cards' do
+        expect{ subject }.to raise_error(ArgumentError, "Should not accept duplicated cards")
+      end
     end
   end
 
-  def test_duplicate_cards
-    assert_raise(ArgumentError, "Should not accept duplicated cards") do
-      TexasHoldEm.new("KD 9H 10D AD JD 6S 9H")
+  shared_examples "returns expected output" do
+    it 'returns best hand' do
+      expect(subject.best_hand).to eq(expected_output)
     end
   end
 
-  ###############
-  #             #
-  # Royal Flush #
-  #             #
-  ###############
+  describe '#best_hand' do
+    context 'royal flush' do
+      let(:cards) { "KD 9H 10D AD JD 6S QD" }
+      let(:expected_output) { "Royal Flush (A high)" }
+      it_behaves_like "returns expected output"
+    end
 
-  def test_royal_flush
-    cards = "KD 9H 10D AD JD 6S QD"
-    assert_equal "Royal Flush (A high)", TexasHoldEm.new(cards).best_hand
+    describe 'straight flush' do
+      let(:cards) { "KD 9H 10D 9D JD 6S QD" }
+      let(:expected_output) { "Straight Flush (K high)"}
+      it_behaves_like "returns expected output"
+    end
+
+    describe 'four of a kind' do
+      let(:cards) { "4C 7D 7H 3S 7C 10H 7S" }
+      let(:expected_output) { "Four of a Kind (7 high)" }
+      it_behaves_like "returns expected output"
+    end
+
+    describe 'full house high card' do
+      let(:cards) { "AH AC 2D 2H 2C 5S 8S" }
+
+      it 'returns best hand' do
+        expect(subject.best_hand).to eq("Full House (2 high)")
+      end
+    end
+
+    describe 'flush beats straight' do
+      let(:cards) { "2D 4D 6D 7C 8C 9D 10D" }
+
+      it 'returns best hand' do
+        expect(subject.best_hand).to eq("Flush (10 high)")
+      end
+    end
+
+    describe 'flush beats straight with face card' do
+      let(:cards) { "2D 4D 6D 7C KC 9D 10D" }
+
+      it 'returns best hand' do
+        expect(subject.best_hand).to eq("Flush (10 high)")
+      end
+    end
+
+    describe 'three of a kind and not full house' do
+      let(:cards) { "AH KC 2D 2H 2C 5S 8S" }
+
+      it 'returns best hand' do
+        expect(subject.best_hand).to eq("Three of a Kind (2 high)")
+      end
+    end
+
+    describe 'pick full house and not three of a kind' do
+      let(:cards) { "KH KC 2D 2H 2C KS 8S" }
+
+      it 'returns best hand' do
+        expect(subject.best_hand).to eq("Full House (K high)")
+      end
+    end
+
+    describe 'straight' do
+      let(:cards) { "2C 4D AH 6S 5D 3C 10S" }
+
+      it 'returns best hand' do
+        expect(subject.best_hand).to eq("Straight (6 high)")
+      end
+    end
+
+    describe 'three of a kind' do
+      let(:cards) { "4C 7D QH 3S 7H 10H 7S" }
+
+      it 'returns best hand' do
+        expect(subject.best_hand).to eq("Three of a Kind (7 high)")
+      end
+    end
+
+    describe 'two pair' do
+      let(:cards) { "4C 7D QH 3S 7H 10H QS" }
+
+      it 'returns best hand' do
+        expect(subject.best_hand).to eq("Two Pair (Q high)")
+      end
+    end
+
+    describe 'two of a kind' do
+      let(:cards) { "4C 7D 2H 3S JD 10H 7S" }
+
+      it 'returns best hand' do
+        expect(subject.best_hand).to eq("Two of a Kind (7 high)")
+      end
+    end
+
+    describe 'high card' do
+      let(:cards) { "4C 7D 2H 3S KD 10H 6S" }
+
+      it 'returns best hand' do
+        expect(subject.best_hand).to eq("High Card (K high)")
+      end
+    end
   end
-
-  ##################
-  #                #
-  # Straight Flush #
-  #                #
-  ##################
-
-  def test_straight_flush
-    cards = "KD 9H 10D 9D JD 6S QD"
-    assert_equal "Straight Flush (K high)", TexasHoldEm.new(cards).best_hand
-  end
-
-  ##################
-  #                #
-  # Four of a Kind #
-  #                #
-  ##################
-
-  def test_pick_four_of_a_kind
-    cards = "4C 7D 7H 3S 7C 10H 7S"
-    assert_equal "Four of a Kind (7 high)", TexasHoldEm.new(cards).best_hand
-  end
-
-  ##############
-  #            #
-  # Full House #
-  #            #
-  ##############
-
-  def test_pick_correct_high_card_for_full_house
-    cards = "AH AC 2D 2H 2C 5S 8S"
-    assert_equal "Full House (2 high)", TexasHoldEm.new(cards).best_hand
-  end
-
-  #########
-  #       #
-  # Flush #
-  #       #
-  #########
-
-  def test_flush_beats_straight
-    cards = "2D 4D 6D 7C 8C 9D 10D"
-    assert_equal "Flush (10 high)", TexasHoldEm.new(cards).best_hand
-  end
-
-  def test_flush_beats_straight_with_face_card
-    cards = "2D 4D 6D 7C KC 9D 10D"
-    assert_equal "Flush (10 high)", TexasHoldEm.new(cards).best_hand
-  end
-
-  def test_pick_three_of_a_kind_and_not_full_house
-    cards = "AH KC 2D 2H 2C 5S 8S"
-    assert_equal "Three of a Kind (2 high)", TexasHoldEm.new(cards).best_hand
-  end
-
-  def test_pick_full_house_and_not_three_of_a_kind
-    cards = "KH KC 2D 2H 2C KS 8S"
-    assert_equal "Full House (K high)", TexasHoldEm.new(cards).best_hand
-  end
-
-  ############
-  #          #
-  # Straight #
-  #          #
-  ############
-
-  def test_straight
-    cards = "2C 4D AH 6S 5D 3C 10S"
-    assert_equal "Straight (6 high)", TexasHoldEm.new(cards).best_hand
-  end
-
-  ###################
-  #                 #
-  # Three of a Kind #
-  #                 #
-  ###################
-
-  def test_pick_three_of_a_kind
-    cards = "4C 7D QH 3S 7H 10H 7S"
-    assert_equal "Three of a Kind (7 high)", TexasHoldEm.new(cards).best_hand
-  end
-
-  ############
-  #          #
-  # Two Pair #
-  #          #
-  ############
-
-  def test_pick_two_pair
-    cards = "4C 7D QH 3S 7H 10H QS"
-    assert_equal "Two Pair (Q high)", TexasHoldEm.new(cards).best_hand
-  end
-
-  #################
-  #               #
-  # Two of a Kind #
-  #               #
-  #################
-
-  def test_pick_two_of_a_kind
-    cards = "4C 7D 2H 3S JD 10H 7S"
-    assert_equal "Two of a Kind (7 high)", TexasHoldEm.new(cards).best_hand
-  end
-
-  #############
-  #           #
-  # High Card #
-  #           #
-  #############
-
-  def test_pick_high_card
-    cards = "4C 7D 2H 3S KD 10H 6S"
-    assert_equal "High Card (K high)", TexasHoldEm.new(cards).best_hand
-  end
-
 end
