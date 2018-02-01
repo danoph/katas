9abe8042f6e48ce4472c3301ccbcecd1014eedb7
Daniel Errante <danoph@gmail.com>
2018-02-01 15:25:31 -0600

    trying to pull out validations into separate validator class

--- texas_hold_em/spec/texas_hold_em_unit_spec.rb
+++ texas_hold_em/spec/texas_hold_em_unit_spec.rb
@@ -1,6 +1,69 @@
 require 'test/unit'
 require_relative '../texas_hold_em'
 
-# unit tests
+#describe TexasHoldEmPlayerCardsBuilder do
+  #subject { described_class.new }
+
+  #describe '#build' do
+    #let(:card_string1) { '2C' }
+    #let(:card_string2) { 'JD' }
+    #let(:cards_string) { [ card_string1, card_string2 ].join(' ') }
+
+    #let(:cards) { [ two_of_clubs, jack_of_diamonds ] }
+    #let(:two_of_clubs) { double 'two of clubs' }
+    #let(:jack_of_diamonds) { double 'jack of diamonds' }
+
+    #it 'returns a bunch of cards' do
+      #expect(Card).to receive(:new).with(card_string1) { two_of_clubs }
+      #expect(Card).to receive(:new).with(card_string2) { jack_of_diamonds }
+
+      #expect(subject.build(cards_string)).to eq(cards)
+    #end
+  #end
+#end
+
+describe TexasHoldEmCardsValidator do
+  let(:card1) { double 'card 1' }
+  let(:card2) { double 'card 2' }
+  let(:card3) { double 'card 3' }
+  let(:card4) { double 'card 4' }
+  let(:card5) { double 'card 5' }
+  let(:card6) { double 'card 6' }
+  let(:card7) { double 'card 7' }
+  let(:card8) { double 'card 8' }
+
+  describe '#validate' do
+    context 'when more than 7 cards passed in' do
+      let(:cards) { [ card1, card2, card3, card4, card5, card6, card7, card8 ] }
+
+      it 'raises error' do
+        expect { subject.validate(cards) }.to raise_error(ArgumentError, "Should not accept more than 7 cards")
+      end
+    end
+
+    context 'when less than 7 cards passed in' do
+      let(:cards) { [ card1, card2, card3, card4, card5, card6 ] }
+
+      it 'raises error' do
+        expect { subject.validate(cards) }.to raise_error(ArgumentError, "Should not accept fewer than 7 cards")
+      end
+    end
+
+    context 'when duplicate cards passed in' do
+      let(:cards) { [ card1, card2, card2, card4, card5, card6, card7 ] }
+
+      it 'raises error' do
+        expect { subject.validate(cards) }.to raise_error(ArgumentError, "Should not accept duplicated cards")
+      end
+    end
+  end
+end
+
 describe TexasHoldEm do
+  subject { described_class.new card_string }
+
+  let(:card_string) { double 'card string' }
+
+  describe '#initialize' do
+  end
 end
--- texas_hold_em/texas_hold_em.rb
+++ texas_hold_em/texas_hold_em.rb
@@ -1,6 +1,16 @@
 require './card'
 require './texas_hold_em_card_validator'
 
+class TexasHoldEmCardsValidator
+  VALID_NUMBER_OF_CARDS = 7
+
+  def validate(cards)
+    raise ArgumentError, "Should not accept more than 7 cards" if cards.length > VALID_NUMBER_OF_CARDS
+    raise ArgumentError, "Should not accept fewer than 7 cards" if cards.length < VALID_NUMBER_OF_CARDS
+    raise ArgumentError, "Should not accept duplicated cards" if cards.length != cards.uniq.length
+  end
+end
+
 class TexasHoldEm
   VALID_NUMBER_OF_CARDS = 7
 
@@ -13,9 +23,8 @@ class TexasHoldEm
       card
     end
 
-    raise ArgumentError, "Should not accept more than 7 cards" if cards.length > VALID_NUMBER_OF_CARDS
-    raise ArgumentError, "Should not accept fewer than 7 cards" if cards.length < VALID_NUMBER_OF_CARDS
-    raise ArgumentError, "Should not accept duplicated cards" if cards.length != cards.uniq.length
+    cards_validator = TexasHoldEmCardsValidator.new
+    cards_validator.validate(cards)
   end
 
   def best_hand
