fed1353e7813f8185c52235bf3f7a87df3d158b3
Daniel Errante <danoph@gmail.com>
2018-02-01 14:08:11 -0600

    throwing error if invalid card passed in

--- texas_hold_em/texas_hold_em.rb
+++ texas_hold_em/texas_hold_em.rb
@@ -1,2 +1,15 @@
 class TexasHoldEm
+  VALID_RANKS = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+  VALID_SUITS = %w(D C S H)
+
+  def initialize(card_string)
+    cards = card_string.split ' '
+
+    cards.each do |card|
+      rank = card[0...-1]
+      suit = card[-1]
+
+      raise ArgumentError, "Should not accept #{ card }" unless VALID_RANKS.include?(rank) && VALID_SUITS.include?(suit)
+    end
+  end
 end
