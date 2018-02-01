f885fd4d1c40dd8a619cbc93bc47290265222459
Daniel Errante <danoph@gmail.com>
2018-02-01 14:22:04 -0600

    making new card class

--- texas_hold_em/texas_hold_em.rb
+++ texas_hold_em/texas_hold_em.rb
@@ -1,3 +1,12 @@
+class Card
+  attr_reader :rank, :suit
+
+  def initialize(card_string)
+    @rank = card_string[0...-1]
+    @suit = card_string[-1]
+  end
+end
+
 class TexasHoldEm
   VALID_RANKS = %w(2 3 4 5 6 7 8 9 10 J Q K A)
   VALID_SUITS = %w(D C S H)
--- texas_hold_em/texas_hold_em_spec.rb
+++ texas_hold_em/texas_hold_em_spec.rb
@@ -1,6 +1,26 @@
 require 'test/unit'
 require './texas_hold_em'
 
+describe Card do
+  subject { described_class.new card_string }
+
+  context 'valid card string' do
+    let(:card_string) { 'KD' }
+
+    describe '#rank' do
+      it 'returns correct rank' do
+        expect(subject.rank).to eq('K')
+      end
+    end
+
+    describe '#suit' do
+      it 'returns correct suit' do
+        expect(subject.suit).to eq('D')
+      end
+    end
+  end
+end
+
 describe TexasHoldEm do
   subject { described_class.new cards }
 
